a2ba8850f43f59c7f76464f66ebfd18d0407c020
currysoda <98086265+currysoda@users.noreply.github.com>
2024-03-11 23:44:22 +0900

    httpRequest-param-body

--- src/main/java/hello/springmvc/basic/request/RequestParamController.java
+++ src/main/java/hello/springmvc/basic/request/RequestParamController.java
@@ -1,14 +1,18 @@
 package hello.springmvc.basic.request;
 
+import hello.springmvc.basic.HelloData;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.io.IOException;
+import java.util.Map;
 
 @Slf4j
 @Controller
@@ -79,6 +83,7 @@ public class RequestParamController {
 	 * /request-param-required
 	 * int age -> null을 int에 입력하는 것은 불가능, 따라서 Integer 변경해야 함(또는 다음에 나오는
 	defaultValue 사용)
+	 * required param이 전달되지 않을 경우 400 Bad Request 오류코드 발생
 	 */
 	@ResponseBody
 	@RequestMapping("/request-param-required")
@@ -87,6 +92,83 @@ public class RequestParamController {
 			@RequestParam(value = "age", required = false) Integer age) {
 		log.info("username={}, age={}", username, age);
 		return "ok";
+	}
+
+	/**
+	 * @RequestParam
+	 * - defaultValue 사용
+	 *
+	 * 참고: defaultValue는 빈 문자의 경우에도 적용
+	 * /request-param-default?username= (defaultValue 적용)
+	 */
+	@ResponseBody
+	@RequestMapping("/request-param-default")
+	public String requestParamDefault(
+			@RequestParam(value = "username", required = true, defaultValue = "guest") String username,
+			@RequestParam(value = "age", required = false, defaultValue = "-1") Integer age) {
+		log.info("username={}, age={}", username, age);
+		return "ok";
+	}
+
+	/**
+	 * @RequestParam Map, MultiValueMap
+	 * Map(key=value)
+	 * MultiValueMap(key=[value1, value2, ...]) ex) (key=userIds, value=[id1, id2])
+	 */
+	@ResponseBody
+	@RequestMapping("/request-param-map")
+	public String requestParamMapping(
+			@RequestParam Map<String, Object> paramMap) {
+//		log.info("username = {}, age = {}", paramMap.get("username"), paramMap.get("age"));
+		paramMap.forEach((key, value) -> { log.info("{} = {}", key, value); });
+
+		return "ok";
+	}
+
+	/**
+	 * multivaluemap 예제
+	 * 반환시 값이 여러개일 경우
+	 * 보통 잘 없는 경우
+	 * multivaluemap 반환값이 값이 여러개일 경우 List<> 로 받기
+	 */
+	@ResponseBody
+	@RequestMapping("/request-param-multivaluemap")
+	public String requestParamMultiMapping(
+			@RequestParam MultiValueMap<String, Object> paramMap) {
+//		log.info("username = {}, age = {}", paramMap.get("username"), paramMap.get("age"));
+		paramMap.forEach((key, value) -> { log.info("{} = {}", key, value); });
+
+		return "ok";
+	}
+
+	/**
+	 * @ModelAttribute 사용
+	 * 참고: model.addAttribute(helloData) 코드도 함께 자동 적용됨, 뒤에 model을 설명할 때 자세히
+	설명
+	 */
+	@ResponseBody
+	@RequestMapping("/model-attribute-v1")
+	public String modelAttributeV1(@ModelAttribute HelloData helloData) {
+
+		log.info("username = {}, age = {}", helloData.getUsername(), helloData.getAge());
+		log.info(helloData.toString());
+
+		return "OK";
+	}
+
+	/**
+	 * @ModelAttribute 생략 가능
+	 * String, int 같은 단순 타입 = @RequestParam
+	 * argument resolver 로 지정해둔 타입 외 = @ModelAttribute
+	 */
+	@ResponseBody
+	@RequestMapping("/model-attribute-v2")
+	public String modelAttributeV2(HelloData helloData) {
+
+		log.info("username = {}, age = {}", helloData.getUsername(), helloData.getAge());
+		log.info(helloData.toString());
+
+		return "OK";
 	}
 
 
